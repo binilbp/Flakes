7c1a2eb52d2fd5f594255e4926db2fe4e855fbaa
13inilb <13inilb@gmail.com>
2025-05-11 13:00:36 +0530

    Add pdf2xl devshell

---
+++ pdf2xl/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "KTUpdf2xl devShell";
+
+  inputs = {
+    nixpkgsUstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs24_11.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgsUnstable , nixpkgs24_11}: {
+
+    let
+      pkgsUnstable = nixpkgsUnstable.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs24_11.legacyPackages.x86_64-linux;
+
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [ 
+          (pkgs.python312.withPackages 
+            (pypkgs: with ps; 
+              [ pip 
+                tabula-py 
+                xlsxwriter 
+                fastapi 
+                multipart 
+                pyarrow
+                uvicorn
+                polars
+                pdfplumber
+              ]
+            )
+          )
+
+          #jre for tabult-py
+          pkgs.temurin-jre-bin
+
+        ];
+      };
+    };
+  };
+}
