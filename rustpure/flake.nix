62103f1100cb95a329377694c21e0ab92fdcf902
binilbp <biniloffcl@gmail.com>
2025-05-24 00:11:26 +0530

    Create Rust Shell

---
+++ rustpure/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Pure Rust Shell";
+
+  inputs = {
+    nixpkgsUnstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs24_11.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgsUnstable , nixpkgs24_11}: 
+
+    let
+      pkgsUnstable = nixpkgsUnstable.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs24_11.legacyPackages.x86_64-linux;
+
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell { #this "pkgs" is command
+        buildInputs = with pkgsUnstable; [ 
+            rustc
+            cargo
+        ];
+      };
+    };
+}
