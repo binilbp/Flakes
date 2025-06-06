5d78fcba09793ca801e81fc5e81a16b0bc6efd6c
binilbp <binilbp@gmail.com>
2025-06-06 18:46:53 +0530

    Create nodejs flake

---
+++ nodejs/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NodeJs 24 shell";
+
+  inputs = {
+    nixpkgsUnstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs25_05.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgsUnstable , nixpkgs25_05}: 
+
+    let
+      pkgsUnstable = nixpkgsUnstable.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs25_05.legacyPackages.x86_64-linux;
+
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell { #this "pkgs" is command
+        buildInputs = with pkgs; [ 
+		nodejs_24;
+        ];
+      };
+    };
+}
